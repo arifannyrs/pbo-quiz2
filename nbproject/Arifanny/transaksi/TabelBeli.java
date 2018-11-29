97eedd160009fa38864882f742ea63347bf8436d
hp <hp@169.254.43.189>
2018-11-30 01:02:11 +0700

    Update nama class Table menjadi TabelBeli

---
+++ nbproject/Arifanny/transaksi/TabelBeli.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arifanny.transaksi;
+
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Arifanny Ramadhan Sukma | arifannyrs@gmail.com
+ */
+public class TabelBeli extends DefaultTableModel {
+    //KOLOM STRING ARRAY UNTUK MENYIMPAN NAMA KOLOM
+    private String [] kolom;
+    
+    //CONSTRUCTOR
+    public TabelBeli(){
+        this.kolom = new String[]{
+            "Nama", "Harga" , "Jumlah"
+        };
+    }
+    
+    //GETTER 
+    public String [] getKolomNama(){
+        return this.kolom;
+    }
+}
