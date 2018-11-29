ecceb4ed1b79b8fab608ec7773cfb7f0ae3023ff
hp <hp@169.254.43.189>
2018-11-29 15:53:55 +0700

    Membuat Class Item yang berisikan getTotal()

---
+++ nbproject/Arifanny/transaksi/Item.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arifanny.transaksi;
+
+/**
+ *
+ * @author Arifanny Ramadhan Sukma | arifannyrs@gmail.com
+ */
+public class Item {
+    private String nama;
+    private float harga;
+    private int jumlah;
+
+    //CONSTRUCTOR DEFAULT
+    public Item() {
+    }
+
+    //CONSTRUCTOR BERPARAMETER NAMA, HARGA, JUMLAH
+    public Item(String nama, float harga, int jumlah) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jumlah = jumlah;
+    }
+    
+    //GETTER SETTER
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public float getHarga() {
+        return harga;
+    }
+
+    public void setHarga(float harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+    //METHOD OUTPUT
+    public String toString(){
+        return this.nama;
+    }
+    
+    //METHOD UNTUK MENGHITUNG HASIL DARI ITEM DIKAI JUMLAH
+    public float getTotal(){
+        return this.harga * this.jumlah;
+    }
+}
